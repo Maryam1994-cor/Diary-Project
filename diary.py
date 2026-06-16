408841d8c2e0da7c1a2dcee586ecac9190bae689
Maryam1994-cor <mhakami@marymount.edu>
2026-06-16 00:16:06 -0400

    Create basic menu

---
+++ diary.py
@@ -0,0 +1,11 @@
+while True:
+    print("\nDiary Menu")
+    print("1. Add diary entry")
+    print("2. View diary entries by date")
+    print("3. Exit")
+
+    choice = input("Choose 1, 2, or 3: ")
+
+    if choice == "3":
+        print("Goodbye")
+        break
